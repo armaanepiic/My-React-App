6ac6ef9048bd2c873965ec5c8b7e9ac6b4fda717
armaanepiic <armaanepiicfb17@gmail.com>
2025-08-03 12:45:36 +0600

    List rendering: MenuItems list

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,21 @@
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+
+  const menuItems = ["Dashboard", "Profile", "Settings", "Help"];
+
+  const menuItem = menuItems.map((item,index) => (<li key={index}>
+    <button className="w-full text-left p-2 rounded hover:bg-opacity-80 transition-colors hover:bg-gray-200 cursor-pointer">{item}</button>
+  </li>));
+
+  // console.log(menuItem);
+
+  return (
+    <aside className="w-64 p-4 transition-colors duration-300 bg-gray-50 text-gray-800">
+      <nav>
+        <h2 className="text-lg font-semibold mb-2">Menu</h2>
+        <ul>
+          {menuItem}
+        </ul>
+      </nav>
+    </aside>
+  );
 }
